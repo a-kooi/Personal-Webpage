75c7565654cca2ac56b98d57ae5298e8d8842e52
Andreas Kooi <andreaskooi@Andreass-MBP.attlocal.net>
2021-04-18 21:42:22 -0700

    Updated Elements

--- Webpage/server.R
+++ Webpage/server.R
@@ -4,9 +4,17 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
+    output$resume <- downloadHandler(
+        filename = "kooi_resume.pdf",
+        content = function(file) {
+            file.copy("www/kooi_resume.pdf", file)
+        }
+    )
+
+    output$pdfview <- renderUI({
+        test <- tags$iframe(style="height:600px; width:100%", src="kooi_resume.pdf")
+        print(test)
     
-
-
-
     })
-
+    })
+    
--- Webpage/ui.R
+++ Webpage/ui.R
@@ -6,6 +6,8 @@ shinyUI(
     fluidPage(
 
     # Application title
+    fluidRow(img(height="100%",width="100%",src="DSC03118-2.jpg")),
+    
     headerPanel("Andreas Kooi", windowTitle = "Kooi Analytics"),
     tabsetPanel(
         selected = "About",
@@ -24,26 +26,44 @@ shinyUI(
                  )
                  ),
                  
-                 column(9, "contents"),
-                 
+                 column(9, 
+                        
+                        
+                        
+
 
                  
                  
                  ),
         
-        tabPanel("Resume", "contents"),
+        tabPanel("Resume", value = "A",
+           
+                 downloadButton("resume"),
+                 column(6,
+                 uiOutput("pdfview"))
+                 ),
+        
         tabPanel("About", 
                  
-                 h1("My Shiny App"),
-                 p(style = "font-family:Impact",
-                   "See other apps in the",
-                   a(href = "www.roblox.com", "Shiny Showcase"),
-                   
-                   img(height=100,width=100,src="http://www.rstudio.com/
-        images/RStudio.2x.png")
-                 )
-                 
+                 column(4, 
+                        img(height="100%",width="100%",src="portrait.png"),
+                        a(href = "https://www.linkedin.com/in/andreas-kooi-2049201a2/", "LinkedIn")
                  ),
+        
+                column(4,
+                       p( "Hi there! I am studying Applied Physics at UC Santa Cruz. Currently I am writing my senior literature review paper on Quantum Annealing and I am
+                       auditing a graduate class on statistical learning in higher dimensions.
+                       In the Fall I will be attending Cal Poly SLO as a part of their Master's in Business Analytics program. I am interested
+                       in using data to provide practical insights for businesses. Aside from my professional work I enjoy cycling, yoga, 
+                          photography, and playing guitar."
+                      #    a(href = "https://youtu.be/f74XJ5lTO6Q", "making videos"),
+                      # "and",
+                      #     a(href = "https://andreaskooi.bandcamp.com/", "creating music."),
+                         )
+                 ),
+                
+
+        ),
         navbarMenu(title = "More",
                    tabPanel("Contact Me", "contents"),
                    tabPanel("What People Say", "contents"))
